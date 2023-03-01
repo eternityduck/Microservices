202bed2b621274c10b4a4a9cecc3a89f285911bd
kivanval <kivanval@gmail.com>
2023-03-01 02:35:31 +0200

    #2 Build framework for product microservice
    Add Dockerfile.

--- products-api/src/main/java/com/example/productsapi/web/ProductController.java
+++ products-api/src/main/java/com/example/productsapi/web/ProductController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
-@RestController("/products")
+@RestController
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
