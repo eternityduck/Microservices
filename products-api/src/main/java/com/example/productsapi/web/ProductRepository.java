97714f3ed33adc775374ec81f12189e0e4a01d3e
kivanval <kivanval@gmail.com>
2023-04-05 20:52:49 +0300

    #2 Add CORS

--- products-api/src/main/java/com/example/productsapi/web/ProductRepository.java
+++ products-api/src/main/java/com/example/productsapi/web/ProductRepository.java
@@ -1,12 +1,14 @@
 package com.example.productsapi.web;
 
 import com.example.productsapi.domain.Product;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends
-        CrudRepository<Product, Long>,
-        PagingAndSortingRepository<Product, Long> {
+        JpaRepository<Product, Long>,
+        JpaSpecificationExecutor<Product> {
 }
