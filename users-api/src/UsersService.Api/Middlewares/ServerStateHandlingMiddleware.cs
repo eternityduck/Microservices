a60123da49157341a0e06fb977671d8933fa281d
NkyCHRN <14nik20@gmail.com>
2023-05-11 21:45:08 +0300

    Added unhealthy POD emulation

---
+++ users-api/src/UsersService.Api/Middlewares/ServerStateHandlingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using UsersService.Business.Interfaces.Providers;
+
+namespace UsersService.Api.Middlewares;
+
+public sealed class ServerStateHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    private readonly IServerStateManager _serverStateManager;
+
+    public ServerStateHandlingMiddleware(RequestDelegate next, IServerStateManager serverStateManager)
+    {
+        _next = next;
+        _serverStateManager = serverStateManager;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (_serverStateManager.ServerState is Business.Enums.ServerState.Slow)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(10));
+        }
+
+        await _next(context);
+    }
+}
---
+++ users-api/src/UsersService.Business/Managers/ServerStateManager.cs
@@ -0,0 +1,9 @@
+﻿using UsersService.Business.Enums;
+using UsersService.Business.Interfaces.Providers;
+
+namespace UsersService.Business.Providers;
+
+public sealed class ServerStateManager : IServerStateManager
+{
+    public ServerState ServerState { get; set; } = ServerState.Regular;
+}
