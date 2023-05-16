f9d94259fe9d21b77fa470c5e8106a7a0ccd6b0c
Chernikov Nikita <14nik20@gmail.com>
2023-05-17 01:02:46 +0300

    Added default user

---
+++ users-api/src/UsersService.Data/Migrations/20230516215553_AddDefaultUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UsersService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "BirthDay", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[] { 1, new DateOnly(2000, 1, 1), "admin@fakemail.com", "Admin", "Admin", "111111111111" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
